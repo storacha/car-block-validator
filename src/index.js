99c07c5ef3a14387ecd2dc9af648304c60fbbf17
Vasco Santos <santos.vasco10@gmail.com>
2023-03-10 12:40:14 +0100

    fix: use map for all codecs

--- src/index.js
+++ src/index.js
@@ -23,57 +23,27 @@ import { equals } from 'uint8arrays'
  * @property {Uint8Array} bytes
  */
 
+export const hashMap = new Map(
+  [
+    sha256, sha512, murmur3128, murmur332, blake2b256, blake2s256,
+    sha3224, sha3256, sha3384, sha3512, shake128, shake256,
+    keccak224, keccak256, keccak384, keccak512
+  ].map(hash => [hash.code, hash])
+)
+
 /**
  * Validates IPLD block bytes.
  * @param {Block} block
  */
 export async function validateBlock (block) {
-  const hasher = getHasher(block.cid.multihash.code)
+  const hasher = hashMap.get(block.cid.multihash.code)
+  if (!hasher) {
+    throw new Error(`multihash code ${block.cid.multihash.code} is not supported`)
+  }
+
   const bytesHash = await hasher.digest(block.bytes)
 
   if (!equals(bytesHash.digest, block.cid.multihash.digest)) {
     throw new Error(`block with cid ${block.cid.toString()} does not have valid bytes`)
-  }
-}
-
-/**
- * @param {number} multihashCode
- */
-function getHasher (multihashCode) {
-  switch (multihashCode) {
-    case sha256.code:
-      return sha256
-    case sha512.code:
-      return sha512
-    case murmur3128.code:
-      return murmur3128
-    case murmur332.code:
-      return murmur332
-    case blake2b256.code:
-      return blake2b256
-    case blake2s256.code:
-      return blake2s256
-    case sha3224.code:
-      return sha3224
-    case sha3256.code:
-      return sha3256
-    case sha3384.code:
-      return sha3384
-    case sha3512.code:
-      return sha3512
-    case shake128.code:
-      return shake128
-    case shake256.code:
-      return shake256
-    case keccak224.code:
-      return keccak224
-    case keccak256.code:
-      return keccak256
-    case keccak384.code:
-      return keccak384
-    case keccak512.code:
-      return keccak512
-    default:
-      throw new Error(`multihash code ${multihashCode} is not supported`)
   }
 }
--- test/test.spec.js
+++ test/test.spec.js
@@ -3,36 +3,12 @@ import * as pb from '@ipld/dag-pb'
 import { CID } from 'multiformats/cid'
 import { CarWriter, CarBlockIterator } from '@ipld/car'
 
-import { sha256, sha512 } from 'multiformats/hashes/sha2'
-import { murmur3128, murmur332 } from '@multiformats/murmur3'
-import { blake2b256 } from '@multiformats/blake2/blake2b'
-import { blake2s256 } from '@multiformats/blake2/blake2s'
-import {
-  sha3224,
-  sha3256,
-  sha3384,
-  sha3512,
-  shake128,
-  shake256,
-  keccak224,
-  keccak256,
-  keccak384,
-  keccak512
-// @ts-expect-error types not well published
-} from '@multiformats/sha3'
-
-import { validateBlock } from '../src/index.js'
-
-const hashers = [
-  sha256, sha512, murmur3128, murmur332, blake2b256, blake2s256,
-  sha3224, sha3256, sha3384, sha3512, shake128, shake256,
-  keccak224, keccak256, keccak384, keccak512
-]
+import { validateBlock, hashMap } from '../src/index.js'
 
 const bytes = pb.encode({ Data: new Uint8Array([1, 2, 3]), Links: [] })
 
-for (const hasher of hashers) {
-  test(`can validate valid blocks and codecs with codec ${hasher.code}`, async t => {
+for (const [code, hasher] of hashMap) {
+  test(`can validate valid blocks and codecs with codec ${code}`, async t => {
     const hash = await hasher.digest(bytes)
     const cid = CID.create(1, pb.code, hash)
 
@@ -49,8 +25,8 @@ for (const hasher of hashers) {
     t.truthy(true)
   })
 
-  test(`throws when validating blocks with bad bytes with codec ${hasher.code}`, async (t) => {
-    const hash = await sha256.digest(bytes)
+  test(`throws when validating blocks with bad bytes with codec ${code}`, async (t) => {
+    const hash = await hasher.digest(bytes)
     const cid = CID.create(1, pb.code, hash)
 
     const { writer, out } = CarWriter.create([cid])
